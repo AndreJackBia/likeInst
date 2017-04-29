68c0b16850d9a6a7d7cb680106f67a6c9514901d
Davide Bolzoni <d.bolzoni@campus.unimib.it>
2017-04-29 19:15:35 +0200

    Add Content HashTag

--- LikesInstagram/app/src/main/java/bollor/likesinstagram/HashTagActivity.java
+++ LikesInstagram/app/src/main/java/bollor/likesinstagram/HashTagActivity.java
@@ -1,5 +1,6 @@
 package bollor.likesinstagram;
 
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -24,6 +25,7 @@ import java.util.List;
 public class HashTagActivity extends AppCompatActivity {
 
     private List<String> myList = new ArrayList<String>();
+    private List<String> myListTite = new ArrayList<String>();
 
 
     @Override
@@ -40,13 +42,47 @@ public class HashTagActivity extends AppCompatActivity {
 
 
     private void populateList() {
-        myList.add("HashTag1");
-        myList.add("Hashtag2");
-        myList.add("Hashtag3");
-        myList.add("Hashtag4");
-        myList.add("Hashtag5");
-        myList.add("Hashtag6");
+        myListTite.add("Most Popular");
+        myList.add("#love #tweegram #photooftheday #20likes #amazing #smile #follow4follow #like4like #look #instalike #igers #picoftheday #food #instadaily #instafollow #followme #girl #iphoneonly #instagood #bestoftheday #instacool #instago #all_shots #follow #webstagram #colorful #style #swag");
 
+        myListTite.add("Very Popular");
+        myList.add("#fun #instagramers #food #smile #pretty #followme #nature #lol #dog #hair #onedirection #sunset #swag #throwbackthursday #instagood #beach #statigram #friends #hot #funny #blue #life #art #instahub #photo #cool #pink #bestoftheday #clouds");
+
+        myListTite.add("Popular");
+        myList.add("#amazing #followme #all_shots #textgram #family #instago #igaddict #awesome #girls #instagood #my #bored #baby #music #red #green #water #harrystyles #bestoftheday #black #party #white #yum #flower #2012 #night #instalove #niallhoran #jj_forum");
+
+        myListTite.add("Weather");
+        myList.add("#nature #sky #sun #summer #beach #beautiful #pretty #sunset #sunrise #blue #flowers #night #tree #twilight #clouds #beauty #light #cloudporn #photooftheday #love #green #skylovers #dusk #weather #day #red #iphonesia #mothernature");
+
+        myListTite.add("Beach");
+        myList.add("#beach #sun #nature #water #TFLers #ocean #lake #instagood #photooftheday #beautiful #sky #clouds #cloudporn #fun #pretty #sand #reflection #amazing #beauty #beautiful #shore #waterfoam #seashore #waves #wave");
+
+        myListTite.add("Sunset");
+        myList.add("#sunset #sunrise #sun #TFLers #pretty #beautiful #red #orange #pink #sky #skyporn #cloudporn #nature #clouds #horizon #photooftheday #instagood #gorgeous #warm #view #night #morning #silhouette #instasky #all_sunsets");
+
+        myListTite.add("Sun");
+        myList.add("#sun #sunny #sunnyday #sunnydays #sunlight #light #sunshine #shine #nature #sky #skywatcher #thesun #sunrays #photooftheday #beautiful #beautifulday #weather #summer #goodday #goodweather #instasunny #instasun #instagood #clearskies #clearsky #blueskies #lookup #bright #brightsun");
+
+        myListTite.add("Summer");
+        myList.add("#summer #summertime #sun #hot #sunny #warm #fun #beautiful #sky #clearskys #season #seasons #instagood #instasummer #photooftheday #nature #TFLers #clearsky #bluesky #vacationtime #weather #summerweather #sunshine #summertimeshine");
+
+        myListTite.add("Winter");
+        myList.add("#winter #cold #holidays #snow #rain #christmas #snowing #blizzard #snowflakes #wintertime #staywarm #cloudy #instawinter #instagood #holidayseason #photooftheday #season #seasons #nature");
+
+        myListTite.add("Dog");
+        myList.add("#dog #dog #puppy #pup #cute #eyes #instagood #dogs_of_instagram #pet #pets #animal #animals #petstagram #petsagram #dogsitting #photooftheday #dogsofinstagram #ilovemydog #instagramdogs #nature #dogstagram #dogoftheday #lovedogs #lovepuppies #hound #adorable #doglover #instapuppy #instadog");
+
+        myListTite.add("Cats");
+        myList.add("#cat #cats #catsagram #catstagram #instagood #kitten #kitty #kittens #pet #pets #animal #animals #petstagram #petsagram #photooftheday #catsofinstagram #ilovemycat #instagramcats #nature #catoftheday #lovecats #furry #sleeping #lovekittens #adorable #catlover #instacat");
+
+        myListTite.add("Selfie");
+        myList.add("#selfie #selfienation #selfies #me #love #pretty #handsome #instagood #instaselfie #selfietime #face #shamelessselefie #life #hair #portrait #igers #fun #followme #instalove #smile #igdaily #eyes #follow");
+
+        myListTite.add("Iphone");
+        myList.add("#iphone #iphoneonly #apple #appleiphone #ios #iphone3g #iphone3gs #iphone4 #iphone5 #technology #electronics #mobile #instagood #instaiphone #phone #photooftheday #smartphone #iphoneography #iphonegraphy #iphoneographer #iphoneology #iphoneographers #iphonegraphic #iphoneogram #teamiphone");
+
+        myListTite.add("Follow");
+        myList.add("#follow #f4f #followme #followforfollow #follow4follow #teamfollowback #followher #followbackteam #followhim #followall #followalways #followback #me #love #pleasefollow #follows #follower #following");
     }
 
     private void populateListView() {
@@ -67,13 +103,17 @@ public class HashTagActivity extends AppCompatActivity {
                 itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
             }
 
-            String currentString = myList.get(position);
+            String currentStringHashtag = myList.get(position);
+            String currentStringTitle = myListTite.get(position);
 
             ImageView imageView = (ImageView) itemView.findViewById(R.id.image_hashtag);
-            imageView.setImageResource(R.drawable.logo);
+            imageView.setImageResource(R.drawable.imageedit_4_9866281557);
 
             TextView makeText = (TextView) itemView.findViewById(R.id.text_hashtag);
-            makeText.setText(currentString);
+            makeText.setText(currentStringHashtag);
+
+            TextView makeTextTitle = (TextView) itemView.findViewById(R.id.text_title);
+            makeTextTitle.setText(currentStringTitle);
 
 
             return itemView;
@@ -85,7 +125,11 @@ public class HashTagActivity extends AppCompatActivity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-                Toast.makeText(HashTagActivity.this, "Click on" + position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(HashTagActivity.this, "Copied", Toast.LENGTH_LONG).show();
+                String copiedStrig = myList.get(position);
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                clipboard.setText(copiedStrig);
+
 
             }
         });
