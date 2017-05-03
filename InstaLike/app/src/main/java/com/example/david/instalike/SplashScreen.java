49485a74a56e8fdd631008459c1d5c2f097cd165
Davide Bolzoni <d.bolzoni@campus.unimib.it>
2017-05-03 23:17:01 +0200

    Add splashscreen and remove back button

---
+++ InstaLike/app/src/main/java/com/example/david/instalike/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.david.instalike;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by david on 28/04/2017.
+ */
+
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splashscreen);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
